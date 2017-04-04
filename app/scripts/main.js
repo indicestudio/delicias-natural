72906fb686725243b084921d9e7f0d7b2be6fbb1
indicestudio <indicestudio@gmail.com>
2017-04-04 10:27:18 -0300

    agregue todo el sitio menos footer y callout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,46 +1,89 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.box').click(function(){
-    // instanciate new modal
-    var modal = new tingle.modal({
-        footer: true,
-        stickyFooter: false,
-        closeLabel: "Close",
-        cssClass: ['custom-class-1', 'custom-class-2'],
-        onOpen: function() {
-            console.log('modal open');
-        },
-        onClose: function() {
-            console.log('modal closed');
-        },
-        beforeClose: function() {
-            // here's goes some logic
-            // e.g. save content before closing the modal
-            return true; // close the modal
-            return false; // nothing happens
-        }
-    });
+  $('.box').click(function (e) {
+    $('section').addClass('filter-backdrop');
+  });
 
-    // set content
-    modal.setContent('<h1>here\'s some content</h1>');
+  $('.modal.fade').click(function () {
+    $('section').removeClass('filter-backdrop');
+  });
 
-    // add a button
-    modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
-        // here goes some logic
-        modal.close();
-    });
+  $('.button-box ul li').click(function () {
+    console.log(this);
+  });
 
-    // add another button
-    modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
-        // here goes some logic
-        modal.close();
-    });
+  /* /////////////////////BARBA JS//////////////// */
+  Barba.Pjax.start();
+  Barba.Prefetch.init();
 
-    // open modal
-    modal.open();
+  var FadeTransition = Barba.BaseTransition.extend({
+    start: function () {
+      Promise
+        .all([this.newContainerLoading, this.fadeOut()])
+        .then(this.fadeIn.bind(this));
+    },
 
-    // close modal
-    modal.close();
-    });
+    fadeOut: function () {
+      return $(this.oldContainer).animate({
+        opacity: 0
+      }).promise();
+    },
 
-})
+    fadeIn: function () {
+
+      var _this = this;
+      var $el = $(this.newContainer);
+
+      $(this.oldContainer).hide();
+
+      $el.css({
+        visibility: 'visible',
+        opacity: 0
+      });
+
+      $el.animate({
+        opacity: 1
+      }, 400, function () {
+        /**
+         * Do not forget to call .done() as soon your transition is finished!
+         * .done() will automatically remove from the DOM the old Container
+         */
+
+        _this.done();
+      });
+    }
+  });
+
+  /**
+   * Next step, you have to tell Barba to use the new Transition
+   */
+
+  Barba.Pjax.getTransition = function () {
+    /**
+     * Here you can use your own logic!
+     * For example you can use different Transition based on the current page or link...
+     */
+
+    return FadeTransition;
+  };
+
+  /* -----------   Instagram Feed ----------------- */
+  var userId = '3860040881';
+  var appToken = '3860040881.a12c878.26faa4846d984563a59c9b23aa846447';
+  $.getJSON({
+    url: 'https://api.instagram.com/v1/users/' + userId + '/media/recent/?access_token=' + appToken,
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+    success: function (e) {
+      var images = e.data[0].images.standard_resolution.url;
+      var post = e.data;
+      post.forEach(function (el, index, array) {
+        var imgURL = el.images.standard_resolution.url;
+        $('section').append('<img src=' + imgURL + '>')
+        console.log(imgURL);
+      });
+
+    }
+  });
+});
